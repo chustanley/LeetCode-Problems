4474ff71b59ae8ab8054451105eb05c41f4d4779
Stanley Chu <110634988+chustanley@users.noreply.github.com>
2023-08-03 10:50:47 -0700

    Time: 82 ms (97.90%), Space: 54.2 MB (58.66%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -6,18 +6,47 @@ var productExceptSelf = function(nums) {
   const n = nums.length;
   const output = new Array(n);
   let productSoFar = 1;
-  
-  // Calculate products to the left of each element
+
+    
+    /*
+    'This loop calculates the product of EACH element -> towards its left'
+    Our argument is: [1, 2, 3, 4]
+    
+    1: [1] // product of before 1 = 1 even though we dont include 'self' -> acts as a place holder 
+    2: [1] // product of before 2 = 1 
+    3: [2] // product of before 3 = 2
+    4: [6] // product of before 4 = 6
+    
+    result = [1, 1, 2, 6]
+    */
+    
+    // Keep in mind, this is also iterating upward 
   for (let i = 0; i < n; i++) {
     output[i] = productSoFar;
     productSoFar *= nums[i];
   }
   
-  // Calculate products to the right of each element and update output
+
+    
+    /*
+    This loop calculates the product of EACH element -> towards its right
+
+    Our argument is: [1, 2, 3, 4]
+    
+    4: [1
+    3: [4]
+    2: [12]
+    1: [24]
+    
+    result = [24, 12, 4, 1]
+    */
+    
+    
+    
   productSoFar = 1;
-  for (let i = n - 1; i >= 0; i--) {
-    output[i] *= productSoFar;
-    productSoFar *= nums[i];
+  for (let i = n - 1; i >= 0; i--) { //n = 3, 2, 1, 0
+    output[i] *= productSoFar; //productsofar = 1, 4, 12, 24 <- this is from line 49
+    productSoFar *= nums[i]; //the reason why this works is because productSoFar acts as an accumulator. 
   }
   
   return output;
