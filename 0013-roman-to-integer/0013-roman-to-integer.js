55bd412a536e4f94b6d79b094a1c9dc694dd4edc
Stanley Chu <110634988+chustanley@users.noreply.github.com>
2023-08-04 18:17:15 -0700

    Time: 119 ms (81.68%), Space: 46.9 MB (76.66%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -5,44 +5,8 @@
 
 
 
-// var romanToInt = function(s) {
-//     var romanNumbers = {
-//         I: 1,
-//         V: 5,
-//         X: 10,
-//         L: 50,
-//         C: 100,
-//         D: 500,
-//         M: 1000
-//     };
-    
-    
-//     var resultNumber = 0;
-    
-//     for (var i = 0; i < s.length; i++) {
-//         var current = s[i];
-//         var after = s[i + 1];
-        
-//         if (romanNumbers[current] < romanNumbers[after]) {
-//             var lessThan = romanNumbers[after] - romanNumbers[current];
-//             resultNumber += lessThan;
-//             i++;
-            
-//         } else {
-//             resultNumber += romanNumbers[current]
-//         }
-
-      
-
-        
-//     }
-    
-//     return resultNumber;
-// };
-
 var romanToInt = function(s) {
-    
-        var romanNumbers = {
+    var romanNumbers = {
         I: 1,
         V: 5,
         X: 10,
@@ -52,10 +16,46 @@ var romanToInt = function(s) {
         M: 1000
     };
     
+    
+    var resultNumber = 0;
+    
+    for (var i = 0; i < s.length; i++) {
+        var current = s[i];
+        var after = s[i + 1];
+        
+        if (romanNumbers[current] < romanNumbers[after]) {
+            var lessThan = romanNumbers[after] - romanNumbers[current];
+            resultNumber += lessThan;
+            i++;
             
-   var value = 0;
-    for (let i = 0; i < s.length; i+=1){
-        romanNumbers[s[i]] < romanNumbers[s[i+1]] ? value -= romanNumbers[s[i]]: value += romanNumbers[s[i]]
+        } else {
+            resultNumber += romanNumbers[current]
+        }
+
+      
+
+        
     }
-    return value
+    
+    return resultNumber;
 };
+
+// var romanToInt = function(s) {
+    
+//         var romanNumbers = {
+//         I: 1,
+//         V: 5,
+//         X: 10,
+//         L: 50,
+//         C: 100,
+//         D: 500,
+//         M: 1000
+//     };
+    
+            
+//    var value = 0;
+//     for (let i = 0; i < s.length; i+=1){
+//         romanNumbers[s[i]] < romanNumbers[s[i+1]] ? value -= romanNumbers[s[i]]: value += romanNumbers[s[i]]
+//     }
+//     return value
+// };
