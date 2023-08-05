8ccb553c9610122eb897d5b140688cb411cef24e
Stanley Chu <110634988+chustanley@users.noreply.github.com>
2023-08-04 18:16:43 -0700

    Time: 110 ms (92.82%), Space: 47.1 MB (65.46%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -5,8 +5,44 @@
 
 
 
+// var romanToInt = function(s) {
+//     var romanNumbers = {
+//         I: 1,
+//         V: 5,
+//         X: 10,
+//         L: 50,
+//         C: 100,
+//         D: 500,
+//         M: 1000
+//     };
+    
+    
+//     var resultNumber = 0;
+    
+//     for (var i = 0; i < s.length; i++) {
+//         var current = s[i];
+//         var after = s[i + 1];
+        
+//         if (romanNumbers[current] < romanNumbers[after]) {
+//             var lessThan = romanNumbers[after] - romanNumbers[current];
+//             resultNumber += lessThan;
+//             i++;
+            
+//         } else {
+//             resultNumber += romanNumbers[current]
+//         }
+
+      
+
+        
+//     }
+    
+//     return resultNumber;
+// };
+
 var romanToInt = function(s) {
-    var romanNumbers = {
+    
+        var romanNumbers = {
         I: 1,
         V: 5,
         X: 10,
@@ -16,26 +52,10 @@ var romanToInt = function(s) {
         M: 1000
     };
     
-    
-    var resultNumber = 0;
-    
-    for (var i = 0; i < s.length; i++) {
-        var current = s[i];
-        var after = s[i + 1];
-        
-        if (romanNumbers[current] < romanNumbers[after]) {
-            var lessThan = romanNumbers[after] - romanNumbers[current];
-            resultNumber += lessThan;
-            i++;
             
-        } else {
-            resultNumber += romanNumbers[current]
-        }
-
-      
-
-        
+   var value = 0;
+    for (let i = 0; i < s.length; i+=1){
+        romanNumbers[s[i]] < romanNumbers[s[i+1]] ? value -= romanNumbers[s[i]]: value += romanNumbers[s[i]]
     }
-    
-    return resultNumber;
+    return value
 };
