792ebde52d24001ea7f9fba25bb28bb57af9faa8
Stanley Chu <110634988+chustanley@users.noreply.github.com>
2023-08-08 08:47:23 -0700

    Time: 57 ms (89.30%), Space: 43.4 MB (96.86%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,45 +11,37 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    /*
-    Input: 2 linkedList 
-    Output: the same linkedlist but a merged one. 
-    Constraint: if both empty, return array. if one empty, return the other.
-    EdgeCase: im wondering if their in order to begin with lmfao.
-    */
-    
-    
-    /*
-        what you can do is create variables that store the linkedlist 
-        
-        check the val to see which one is least 
-        
-        for the least one, 
 
-    */
     
     
     
- const prehead = {
+ const starter = {
     val: -1,
     next: null
   };
 
-    let prev = prehead;
+
+ let lastNode = starter;
+    
     while (l1 !== null && l2 !== null) {
-      if (l1.val <= l2.val) {
-        prev.next = l1;
-        l1 = l1.next;
-      } else {
-        prev.next = l2;
-        l2 = l2.next;
-      }
-      prev = prev.next;
+        if (l1.val <= l2.val) {
+            lastNode.next = l1;
+            l1 = l1.next;
+        } else {
+            lastNode.next = l2;
+            l2 = l2.next;
+        }
+        
+        lastNode = lastNode.next;
     }
-
-    // At least one of l1 and l2 can still have nodes at this point, so connect
-    // the non-null list to the end of the merged list.
-    prev.next = l1 === null ? l2 : l1;
-
-    return prehead.next;
+    
+    
+    if (l1 === null) {
+        lastNode.next = l2
+    } else {
+        lastNode.next = l1
+    }
+    
+    return starter.next;
+    
 };
